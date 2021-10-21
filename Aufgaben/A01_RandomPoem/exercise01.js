5da3e03d4b35010461a7c7ef77274da32d45c5f3
LenaLappe <80837514+LenaLappe@users.noreply.github.com>
2021-10-21 10:23:10 +0200

    first try

--- Aufgaben/A01_RandomPoem/exercise01.js
+++ Aufgaben/A01_RandomPoem/exercise01.js
@@ -1,16 +1,17 @@
 var A01_RandomPoem;
 (function (A01_RandomPoem) {
     // Bearbeitet mit Liz und Kriss und Amélie
-    var subject = ["Harry", "Hermine", "Ron", "Snape", "Hagrid", "Dumbledore"];
-    var verb = ["braut", "liebt", "studiert", "schreit", "lacht", "zaubert"];
-    var object = ["Zaubertränke", "Hogwarts", "Dementoren", "den Grimm", "Lupin", "Dobby"];
+    var subjects = ["Harry", "Hermine", "Ron", "Snape", "Hagrid", "Dumbledore"];
+    var verbs = ["braut", "liebt", "studiert", "schreit", "lacht", "zaubert"];
+    var objects = ["Zaubertränke", "Hogwarts", "Dementoren", "den Grimm", "Lupin", "Dobby"];
     // console.log(subject);
     // console.log(verb);
     // console.log(object);
-    for (var i = subject.length; i > 0; i--) {
-        getVerse(subject, verb, object);
+    for (var i = subjects.length; i > 0; i--) {
+        var endVers = getVerse(subjects, verbs, objects);
         // console.log(i);
         // console.log(y);
+        console.log(endVers);
     }
     function getVerse(_subject, _verb, _object) {
         var vers = "";
@@ -18,7 +19,6 @@ var A01_RandomPoem;
         var randomNumberV = Math.floor(Math.random() * _verb.length);
         var randomNumberO = Math.floor(Math.random() * _object.length);
         vers += _subject.splice(randomNumberS, 1)[0] + " " + _verb.splice(randomNumberV, 1)[0] + " " + _object.splice(randomNumberO, 1);
-        console.log(vers);
         // console.log(_subject.splice(randomNumberS));
         return ("Alohomora");
     }
